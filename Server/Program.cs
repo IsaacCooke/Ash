2228b25a855105405b1ecdf6680b9b367979092b
Azrael-Studios <cookeisaac2@gmail.com>
2022-12-18 14:34:21 +0000

    Added services and controllers for user

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,4 +1,5 @@
 using Server.Models;
+using Server.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -6,6 +7,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.Configure<DatabaseSettings>(
     builder.Configuration.GetSection("Database")
 );
+
+builder.Services.AddSingleton<UserService>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
