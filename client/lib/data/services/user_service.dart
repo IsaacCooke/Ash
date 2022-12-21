f48009483c0c939834ceec722c9dda762c35912e
Azrael-Studios <cookeisaac2@gmail.com>
2022-12-21 09:20:29 +0000

    Added page files and color scheme

--- client/lib/data/services/user_service.dart
+++ client/lib/data/services/user_service.dart
@@ -10,6 +10,7 @@ class UserService{
     final response = await http.get(
       Uri.parse(ApiConstants.baseUrl + ApiConstants.usersEndpoint)
     );
+    
     if(response.statusCode == 200){
       List<User> _users = userFromJson(response.body);
       print(_users);
@@ -23,6 +24,7 @@ class UserService{
     final response = await http.get(
       Uri.parse(ApiConstants.baseUrl + ApiConstants.usersEndpoint + '/$id')
     );
+
     if(response.statusCode == 200){
       print(response);
       return User.fromJson(jsonDecode(response.body));
@@ -52,12 +54,42 @@ class UserService{
         'reminders': newUser.reminders,
       }),
     );
+
     if(response.statusCode == 201){
       return User.fromJson(jsonDecode(response.body));
     } else {
       throw Exception("Failed to create user");
     }
   }
+
+  Future<User> updateUser(int id, User updatedUser) async {
+    final response = await http.put(
+      Uri.parse(ApiConstants.baseUrl + ApiConstants.usersEndpoint + "/$id"),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: jsonEncode(<String, dynamic>{
+        'id': updatedUser.id,
+        'firstName': updatedUser.firstName,
+        'lastName': updatedUser.lastName,
+        'email': updatedUser.email,
+        'password': updatedUser.password,
+        'phoneNumber': updatedUser.phoneNumber,
+        'hasAgreedToConditions': updatedUser.hasAgreedToConditions,
+        'language': updatedUser.language,
+        'address': updatedUser.address.toJson(),
+        'settings': updatedUser.settings.toJson(),
+        'birthDate': updatedUser.birthDate,
+        'reminders': updatedUser.reminders,
+      }),
+    );
+
+    if (response.statusCode == 200) {
+      return User.fromJson(json.decode(response.body));
+    } else {
+      throw Exception('Failed to update album.');
+    }
+}
 
   Future<User> deleteUser(int id) async {
     final http.Response response = await http.delete(
--- client/lib/main.dart
+++ client/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:client/themes/color_schemes.g.dart';
 
 void main() {
   runApp(const MyApp());
@@ -25,11 +26,16 @@ class MyApp extends StatelessWidget {
         Locale('es', ''), // Generic Spanish
       ],
       home: MyHomePage(),
+      theme: ThemeData(useMaterial3: true, colorScheme: lightColorScheme),
+      darkTheme: ThemeData(useMaterial3: true, colorScheme: darkColorScheme),
     );
   }
 }
 
 class MyHomePage extends StatelessWidget{
+  const MyHomePage({super.key});
+
+  @override
   Widget build(BuildContext context){
     return Column(
       children: [
