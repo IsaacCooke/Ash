49433ddcb03afa168b0692131acf561d24848038
Azrael-Studios <cookeisaac2@gmail.com>
2022-12-29 08:08:00 +0000

    Made T&C and EULA

--- client/lib/widgets/terms_and_conditions.dart
+++ client/lib/widgets/terms_and_conditions.dart
@@ -16,13 +16,39 @@ class TermsAndConditions extends StatelessWidget{
       body: Padding(
         padding: const EdgeInsets.all(30),
         child: Column(
-          children: const <Widget>[
-            Text(
+          children: <Widget>[
+            const Text(
               "Please read before using this product",
               style: titleStyle,
             ),
-            Text(
-              ""
+            const Text(
+              "By using this product, you must agree to the following conditions:"
+            ),
+            const ListTile(
+              leading: Icon(Icons.fiber_manual_record),
+              title: Text("You accept that this product can receive fully uncensored material from the internet and that material may be used by the product or shown to you")
+            ),
+            const ListTile(
+              leading: Icon(Icons.fiber_manual_record),
+              title: Text("You accept that this product is still in development and its features may not be complete and may change before the official release"),
+            ),
+            const ListTile(
+              leading: Icon(Icons.fiber_manual_record),
+              title: Text("You accept that any data entered into this app can be accessed by the designer, Isaac Cooke, and potentially AQA Exam Board and JCQ regulators"),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 8.0),
+              child: Column(
+                children: const <Widget>[
+                  Text(
+                    "End User License Agreement",
+                    style: titleStyle,
+                  ),
+                  Text("This copy of ASH (\"the product\") and accompanying documentation is licenced and not sold. This product is protected by copyright laws and treaties, as well as laws and treaties relating to other forms of interlecutal property" +
+                  "Isaac Cooke owns interlecutal property rights to the product. The Licensee's (\"you\" or \"your\") license to download, use, copy or change the product is subject to these rights and to all te terms and conditions in this end user license agreement (\"agreement\")."
+                  ),
+                ],
+              ),
             )
           ],
         ),
