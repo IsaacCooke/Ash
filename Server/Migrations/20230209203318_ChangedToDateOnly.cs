6ae5baf8634f7558bcd69313fd5ad10759568503
Azrael-Studios <cookeisaac2@gmail.com>
2023-02-25 17:22:47 +0000

    Cleanup

---
+++ Server/Migrations/20230209203318_ChangedToDateOnly.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangedToDateOnly : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateOnly>(
+                name: "BirthDate",
+                table: "User",
+                type: "date",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "BirthDate",
+                table: "User",
+                type: "timestamp with time zone",
+                nullable: false,
+                oldClrType: typeof(DateOnly),
+                oldType: "date");
+        }
+    }
+}
